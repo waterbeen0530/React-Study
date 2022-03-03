c79b2dfec900d4cc111be358d97411e3183e1225
waterbeen0530 <hange253@gmail.com>
2022-03-03 16:35:55 +0900

    [CREATE] Component
    React - Component 연습

--- React-Class/blog/src/App.js
+++ React-Class/blog/src/App.js
@@ -38,8 +38,22 @@ function App() {
         <p>3월 3일 발행</p>
         <hr/>
         </div>
+
+        <Modal />
+
     </div>
   );
+
+  function Modal(){
+    return (
+      <div className='modal'>
+          <h2>제목</h2>
+          <p>날짜</p>
+          <p>상세내용</p>
+      </div>
+    )
+  }
+
 }
 
 export default App;
