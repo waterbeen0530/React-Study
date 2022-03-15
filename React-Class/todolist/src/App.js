c96cc08426641a7daaba7c9049c8dc243cd81f1f
waterbeen0530 <hange253@gmail.com>
2022-03-15 19:54:37 +0900

    [CREATE] Counter
    react - 카운터 만들기

--- React-Class/todolist/src/App.js
+++ React-Class/todolist/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial={true}/>
-      {/* 만약 isSpecial 이름만 넣어주면 isSpecial={true}가 됨 */}
-      <Hello color="pink"/>
-    </Wrapper>
+    <Counter />
   );
 }
 
