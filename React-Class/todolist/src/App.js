030aa870a91f431134da554a91e2444dc07165eb
waterbeen0530 <hange253@gmail.com>
2022-03-15 19:17:07 +0900

    [STUDY] props
    react - props 연습

--- React-Class/todolist/src/App.js
+++ React-Class/todolist/src/App.js
@@ -1,19 +1,13 @@
-import React from "react";
-import Hello from "./Hello";
-import Bye from "./Bye";
-import './App.css';
+import React from 'react';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
 
 function App() {
-  const name = 'react';
   return (
-    <div>
-      {/* 주석처리 */}
-      /* 중괄호로 감싸지 않으면 화면에 보임 */
-      <Hello />
-      <Bye />
-      <div className="Add">{name}</div>
-      <div className="gray-box"></div>
-    </div>
+    <Wrapper>
+      <Hello name="react" color="red"/>
+      <Hello color="pink"/>
+    </Wrapper>
   );
 }
 
--- React-Class/todolist/src/Bye.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Bye() {
-  return <div>안녕계세요</div>
-}
-
-export default Bye;
