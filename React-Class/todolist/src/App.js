6d1928fa4cd8afa9a7be2518bf4396b5e9547f6e
waterbeen0530 <hange253@gmail.com>
2022-03-15 17:25:10 +0900

    [STUDY] JSX
    react - JSX 연습

--- React-Class/todolist/src/App.js
+++ React-Class/todolist/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Hello from "./Hello";
 import Bye from "./Bye";
+import './App.css';
 
 function App() {
+  const name = 'react';
   return (
     <div>
+      {/* 주석처리 */}
+      /* 중괄호로 감싸지 않으면 화면에 보임 */
       <Hello />
       <Bye />
-      <Hello />
-      <Bye />
+      <div className="Add">{name}</div>
+      <div className="gray-box"></div>
     </div>
   );
 }
