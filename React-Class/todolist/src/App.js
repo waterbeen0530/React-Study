3f776a422a493680ab89bf3a7d866339e7e6c2d3
waterbeen0530 <hange253@gmail.com>
2022-03-15 17:10:17 +0900

    [STUDY] Component
    react - 컴포넌트 연습

---
+++ React-Class/todolist/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hello from "./Hello";
+import Bye from "./Bye";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
---
+++ React-Class/todolist/src/Bye.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Bye() {
+  return <div>안녕계세요</div>
+}
+
+export default Bye;
