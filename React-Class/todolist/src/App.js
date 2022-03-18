881beb7d0b7fa5f9f80be77f07e29b8ca7803283
waterbeen0530 <hange253@gmail.com>
2022-03-18 15:53:09 +0900

    [STUDY]  Add items to array
    react - 배열에 항목 추가하기

--- React-Class/todolist/src/App.js
+++ React-Class/todolist/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import Counter from './Counter';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+
+  const [inputs, setInputs] = useState({
+    username: '',
+    email:''
+  });
+
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'lim',
@@ -21,23 +36,37 @@ function App() {
       username: 'bin',
       email: 'bin@gmail.com'
     }
-  ];
+  ]);
 
-  const nextId = useRef(4);
+  const nextId = useRef(4)
   const onCreate = () => {
-    //나중에 구현 할 배열에 항목 추가하는 로직
-    //...
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
 
+    setUsers([...users, user]);
+
+    setInputs({
+      username:'',
+      email:''
+    })
     nextId.current += 1;
-  };
+  }
 
   return (
     <>
-      <UserList users={users} />;
-      <Counter2 />
-    </>
-  )
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
 
-}
+      <UserList users={users} />
+    </>
+  );
+};
 
 export default App;
