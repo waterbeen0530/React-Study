4a8e66da101d7ed7d69eb2733e3a90733e2ec904
waterbeen0530 <hange253@gmail.com>
2022-04-07 23:14:43 +0900

    [UPDATE] Ideal Type Worldcup
    Router 추가

--- ideal-type-worldcup/src/components/Header.jsx
+++ ideal-type-worldcup/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from './Header.module.css';
+import { Link } from "react-router-dom";
 
 function Header () {
   return (
     <div className={styles.container}>
     <header className={styles.header}>
-      <p className={styles.main_logo}>🏆MAIN</p>
+      <Link to="/">
+        <p className={styles.main_logo}>🏆MAIN</p>
+      </Link>
       <div className={styles.header_menu}>
         <p id={styles.header_menu1}>이상형 월드컵</p>
         <p id={styles.header_menu2}>이상형 월드컵 만들기</p>
